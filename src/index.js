29f3b6eee1b407da197fcda53be085815c2b44aa
MarioTurco <mario.turco4@studenti.unina.it>
2020-08-31 16:51:05 +0200

    modifiche

--- src/index.js
+++ src/index.js
@@ -31,7 +31,10 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', function(){
+    createWindow();
+    autoUpdater.checkForUpdatesAndNotify();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
