23e0ae41aa5e020c3c266de2783546be06dd9340
MarioTurco <mario.turco4@studenti.unina.it>
2020-08-31 20:07:05 +0200

    Aggiunto grafico peso

--- src/visualizeLogic.js
+++ src/visualizeLogic.js
@@ -19,9 +19,46 @@ settingsTab.onclick = () => {
     ipcRenderer.sendSync('synchronous-message', 'settings');
 };
 
+//Canvas
+var ctx = document.getElementById('chart').getContext('2d');
+
+
+function sortDataByDate(data){
+    return( data.sort((a,b) => a.data < b.data ? -1 : 1));
+}
+function createChart(data){
+    sortDataByDate(data);
+    let peso = []
+    let date = []
+    for(elems in data){
+        peso.push(data[elems].peso);
+        date.push(data[elems].data);
+    }
+    console.log(data);
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: date,
+            datasets: [{
+                label: 'Peso Corporeo',
+                //backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: peso
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+}
+
 function fillTable(){
+    
     db.getAll('peso',(succ, data) => {
         console.log(data);
+        createChart(data);
         let index = 1;
         for(elems in data){
             var row = table.insertRow(index);
